0f4c93a38b7aa9e631e33bfabf4294be30d0a3af
SethCol99 <sd17colu@siena.edu>
2022-11-29 20:18:15 -0500

    did this finally work

--- firstdj_4real/subatomics/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ElectronsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'electrons'
--- firstdj_4real/subatomics/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.atoms),
-    path('electrons', views.electrons),
-    path('protons', views.protons),
-    path('neutrons', views.neutrons)
-]
